52e5973d9003e89c7a4d008714b9aa222d08329f
Alex Lakatos <alex.lakatos@ovoenergy.com>
2015-10-29 17:53:10 +0000

    added generated service to move over audio data

---
+++ app/scripts/services/fartservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name firefartApp.fartService
+ * @description
+ * # fartService
+ * Service in the firefartApp.
+ */
+angular.module('firefartApp')
+  .service('fartService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
